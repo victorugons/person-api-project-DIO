2f367612b657bebc83bcfbee69dede3b7fdf911e
victorugons <victorugons@gmail.com>
2021-06-13 17:55:59 -0300

    Create project initial structure

---
+++ src/test/java/dev/victorugons/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.victorugons.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
