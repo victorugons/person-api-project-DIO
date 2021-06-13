10107d92c5da34f01d64b47d3beb4d66aadbe7e1
victorugons <victorugons@gmail.com>
2021-06-13 17:56:44 -0300

    Create Phone and Person entities and PhoneType enum

---
+++ src/main/java/dev/victorugons/personapi/entity/Phone.java
@@ -0,0 +1,31 @@
+package dev.victorugons.personapi.entity;
+
+import dev.victorugons.personapi.enums.PhoneType;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false, unique = true)
+    private String number;
+}
